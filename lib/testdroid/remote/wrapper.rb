22daf55589f9f70508cdcd0f22a3d9843e0d68ef
Sakari Rautiainen <sakari.rautiainen@bitbar.com>
2014-04-25 14:34:44 -0700

    Initial commit

---
+++ lib/testdroid/remote/wrapper.rb
@@ -0,0 +1,9 @@
+require "testdroid/remote/wrapper/version"
+
+module Testdroid
+  module Remote
+    module Wrapper
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/testdroid/remote/wrapper/version.rb
@@ -0,0 +1,7 @@
+module Testdroid
+  module Remote
+    module Wrapper
+      VERSION = "0.0.1"
+    end
+  end
+end
